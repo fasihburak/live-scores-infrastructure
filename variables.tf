a90fc78f6375f8aa199be26b3d34dfe2bf55ff7b
Fasih Burak <fasihburak@gmail.com>
2025-07-16 17:40:38 +0300

    Remove all resources

--- variables.tf
+++ variables.tf
@@ -12,15 +12,15 @@ variable "AWS_REGION" {
   type        = string
 }
 
-variable "instance_name" {
-  description = "Value of the EC2 instance's Name tag."
-  type        = string
-  default     = "learn-terraform"
-}
+# variable "instance_name" {
+#   description = "Value of the EC2 instance's Name tag."
+#   type        = string
+#   default     = "learn-terraform"
+# }
 
-variable "instance_type" {
-  description = "The EC2 instance's type."
-  type        = string
-  default     = "t2.micro"
-}
+# variable "instance_type" {
+#   description = "The EC2 instance's type."
+#   type        = string
+#   default     = "t2.micro"
+# }
 
