39737cec1c81a5384287ff800c5a7411ac96379e
Fasih Burak <fasihburak@gmail.com>
2025-07-22 16:50:17 +0300

    Fix uncomment alb_sg

--- main.tf
+++ main.tf
@@ -151,13 +151,13 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 # module "alb" {
 #   source  = "terraform-aws-modules/alb/aws"
