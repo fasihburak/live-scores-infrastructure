bdedef8689c616173234d8d788be6acf477d3051
Fasih Burak <fasihburak@gmail.com>
2025-07-31 16:16:49 +0300

    Give EC2 access to ECR

--- main.tf
+++ main.tf
@@ -64,6 +64,30 @@ resource "aws_ecr_repository" "django" {
   }
 }
 
+resource "aws_iam_role" "ecr_access_for_ec2" {
+  name = "ECR-Access-for-EC2-Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_access_for_ec2" {
+  role       = aws_iam_role.ecr_access_for_ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ecr_access_for_ec2" {
+  name = "ecr-access-for-ec2-profile"
+  role = aws_iam_role.ec2_ecr.name
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "6.0.1"
@@ -273,11 +297,12 @@ module "ec2_instance" {
   monitoring    = true
   subnet_id     = module.vpc.public_subnets[0]
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ecr_access_for_ec2.name
   # Install Docker on EC2
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
-    sudo amazon-linux-extras install docker -y
+    sudo yum install -y docker
     sudo service docker start
     sudo usermod -aG docker ec2-user
   EOF
